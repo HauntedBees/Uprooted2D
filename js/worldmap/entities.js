cc92e16f898e2cb0f3594847d7be2f7ac81180e6
Sean Finch <fench@hauntedbees.com>
2017-07-24 22:09:50 -0700

    that behaved weirdly!

--- js/worldmap/entities.js
+++ js/worldmap/entities.js
@@ -206,12 +206,16 @@ var mapentities = {
         GetCommonEntity("Worker2", 20, 10, 0, 3, GetStdMovement([ [20, 10, 1], [10, 10, 1], [10, 8, 0], [20, 8, 3], [20, 10, 2] ]), [
             function(activePress) {
                 if(player.hasQuest("helpSeaMonster")) {
-                    combat.startBattle(["Worker"]);
+                    worldmap.writeText("get fucked four eyes");
                 } else if(activePress) {
                     worldmap.writeText("hi im paul");
+                    worldmap.forceEndDialog = true;
                 } else {
                     return true;
                 }
+            },
+            function() {
+                combat.startBattle(["Worker"]);
             }
         ])
     ],
