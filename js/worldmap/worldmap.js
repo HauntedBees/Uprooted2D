ac00e339d302680a010d52a1e7b6523304af31ef
Sean Finch <fench@hauntedbees.com>
2017-10-14 17:05:22 -0700

    the rest of noclip lol

--- js/worldmap/worldmap.js
+++ js/worldmap/worldmap.js
@@ -242,7 +242,7 @@ var worldmap = {
                 }
             }
         }
-        if(!hasCollisions) { this.pos = pos; }
+            if(!hasCollisions) { this.pos = pos; }
         if(isEnter) {
             switch(this.playerDir) {
                 case 0: newPos.y--; break;
