98340d20424d4386ddcb033b2df4e110563b3fa9
Sean Finch <fench@hauntedbees.com>
2018-02-20 23:01:11 -0800

    resized falcon and inventory cleaning menus.

--- js/worldmap/falconSelect.js
+++ js/worldmap/falconSelect.js
@@ -1,13 +1,13 @@
 worldmap.falconSelect = {
     cursor: { x: 0, y: 0 }, cropsToSend: [], 
     layersToClean: ["menuA", "menuB", "menucursorA", "menucursorB", "menutext"],
-    actualIndexes: [], inventoryWidth: 9, dy: 6,
+    actualIndexes: [], inventoryWidth: 9, dy: 9.5,
     setup: function() {
         this.cursor = { x: 0, y: 0 };
         this.cropsToSend = [];
         this.actualIndexes = [];
-        var validTypes = ["veg", "tree", "mush", "rice"];
-        for(var i = 0; i < player.inventory.length; i++) {
+        const validTypes = ["veg", "tree", "mush", "rice"];
+        for(let i = 0; i < player.inventory.length; i++) {
             if(player.inventory[i][0][0] === "_" || player.inventory[i][0][0] === "!") { continue; }
             if(validTypes.indexOf(GetCrop(player.inventory[i][0]).type) < 0) { continue; }
             this.actualIndexes.push(i);
@@ -17,33 +17,34 @@ worldmap.falconSelect = {
 
     drawAll: function() {
         gfx.clearSome(this.layersToClean);
-        var cursorX = this.cursor.x, cursorY = this.cursor.y + 6;
+        const cursorX = this.cursor.x, cursorY = this.cursor.y + this.dy;
         worldmap.writeText("falconSelect");
         this.setText();
-        gfx.drawInfobox(16, 3, this.dy + 1);
-        gfx.drawInfobox(6, 3, this.dy + 1);
-        gfx.drawInfobox(16, 2, 1.5);
+        // top
+        gfx.drawInfobox(17, 2, 1.5);
         gfx.drawFullText(GetText("falconSeeds"), 32);
-        for(var i = 0; i < this.actualIndexes.length; i++) {
-            var actItem = player.inventory[this.actualIndexes[i]];
-            gfx.drawInventoryItem(actItem, i % this.inventoryWidth, this.dy + 1 + Math.floor(i / this.inventoryWidth), "menuA");
-        }
-        for(var i = 0; i < this.cropsToSend.length; i++) {
+        for(let i = 0; i < this.cropsToSend.length; i++) {
             gfx.drawTileToGrid(this.cropsToSend[i] + "seed", 2.5 + i, 2, "menuA");
         }
-        //this.options = [];
-        var xi = this.drawOption(GetText("falconConfirm"), 3.25);
+        // bottom
+        gfx.drawInfobox(17, 5, this.dy);
+        gfx.drawInfobox(7, 5, this.dy);
+        for(let i = 0; i < this.actualIndexes.length; i++) {
+            const actItem = player.inventory[this.actualIndexes[i]];
+            gfx.drawInventoryItem(actItem, i % this.inventoryWidth, this.dy + Math.floor(i / this.inventoryWidth), "menuA");
+        }
+
+        const xi = this.drawOption(GetText("falconConfirm"), 3.25);
         if(this.cursor.y < 0) {
             gfx.drawCursor(0, 3.25, xi, 0);
         } else {
-            gfx.drawCursor(cursorX, cursorY + 1, 0, 0);
+            gfx.drawCursor(cursorX, cursorY, 0, 0);
         }
     },
     drawOption: function(text, y, selected) {
-        var xi = 1, tile = 7;
-        if(selected) { tile = 9; }
+        let xi = 1, width = gfx.getTextWidth(text);
+        const tile = (selected ? 9 : 7);
         gfx.drawSprite("sheet", tile, 11, 0, 2 + y * 16, "menuA");
-        var width = gfx.getTextWidth(text);
         while(width > 128) {
             width -= 64;
             gfx.drawSprite("sheet", tile, 11, 16 * xi++, 2 + y * 16, "menuA");
@@ -51,34 +52,65 @@ worldmap.falconSelect = {
         gfx.drawSprite("sheet", tile + 1, 11, 16 * xi, 2 + y * 16, "menuA");
         gfx.drawText(text, 2, 10.5 + y * 16);
         return xi;
-        //this.options.push(xi);
     },
     setText: function() {
         if(this.cursor.y < 0) {
-            var sendText = HandlePlurals(GetText("falconConfirmX"), this.cropsToSend.length).replace("{0}", this.cropsToSend.length);
+            const sendText = HandlePlurals(GetText("falconConfirmX"), this.cropsToSend.length).replace("{0}", this.cropsToSend.length);
             gfx.drawWrappedText(sendText, 9.5 * 16, 11 + (16 * (this.dy + 1)), 85);
             return;
         }
-        var idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
-        var item = player.inventory[this.actualIndexes[idx]];
+        const idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
+        const item = player.inventory[this.actualIndexes[idx]];
         if(item === null || item === undefined) { return; }
-        var iteminfo = GetCrop(item[0]);
-        var str = iteminfo.displayname + " (" + item[1] + ")\n";
-        str += " Power: " + iteminfo.power + "\n";
-        if(iteminfo.time > 0) { str += " Time: " + Math.ceil(iteminfo.time / player.getCropSpeedMultiplier()) + "\n"; }
-        if(iteminfo.respawn > 0) { str += " Regrowth: " + iteminfo.respawn + "\n"; }
-        if(iteminfo.seasons[0] > 0.5) { str += " SP"; }
-        if(iteminfo.seasons[1] > 0.5) { str += " SU"; }
-        if(iteminfo.seasons[2] > 0.5) { str += " AU"; }
-        if(iteminfo.seasons[3] > 0.5) { str += " WI"; }
-        gfx.drawWrappedText(str, 9.5 * 16, 11 + (16 * (this.dy + 1)), 85);
+
+        const crop = GetCrop(item[0]);
+        let str = "x" + item[1] + " " + crop.displayname;
+        pausemenu.inventory.DrawCropPower(crop, 9.5, 10.5, "menutext");
+        const row2y = 11.5;
+        let leftMostX = 9.5;
+        if(crop.time > 0) {
+            gfx.drawTileToGrid("inv_time", leftMostX, row2y, "menutext");
+            if(crop.time === 999) { // TODO: -1 vs 999 what is the diff?
+                gfx.drawTileToGrid("bigNum?", leftMostX + 1, row2y, "menutext");
+            }  else {
+                gfx.drawBigNumber(crop.time, leftMostX + 1, row2y, "menutext");
+            }
+            leftMostX += 2;
+        }
+        let maxBonuses = 4;
+        if(crop.respawn > 0) {
+            gfx.drawTileToGrid("inv_regrow", leftMostX, row2y, "menutext");
+            if(crop.respawn === 999 || crop.respawn === -1) {
+                gfx.drawTileToGrid("bigNum?", leftMostX + 1, row2y, "menutext");
+            }  else {
+                gfx.drawBigNumber(crop.respawn, leftMostX + 1, row2y, "menutext");
+            }
+            leftMostX += 2;
+            maxBonuses = 2;
+        }
+        let bonusesToPush = [];
+        if(crop.waterResist) { bonusesToPush.push("waterIco" + crop.waterResist); }
+        if(crop.fireResist) { bonusesToPush.push("fireIco" + crop.fireResist); }
+        if(crop.stickChance) { bonusesToPush.push("stunIco" + crop.stickChance); }
+        if(crop.saltResist) { bonusesToPush.push("saltIco" + crop.saltResist); }
+        if(crop.saltClean) { bonusesToPush.push("saltIcoX"); }
+        if(crop.animal) { bonusesToPush.push("animal" + crop.animal); }
+        leftMostX += 0.5;
+        for(let i = 0; i < Math.min(bonusesToPush.length, maxBonuses); i++) {
+            gfx.drawTileToGrid(bonusesToPush[i], leftMostX + i, row2y, "menutext");
+        }
+        const seasons = ["spring", "summer", "autumn", "winter"];
+        for(let i = 0; i < 4; i++) {
+            gfx.drawTileToGrid(seasons[i] + crop.seasons[i], 9.5 + i, 12.75, "menutext");
+        }
+        gfx.drawWrappedText(str, 9.5 * 16, 11 + (16 * this.dy), 115);
     },
 
     mouseMove: function(pos) {
         if(pos.x < 0 || pos.y < -1) { return false; }
         if(pos.x >= this.inventoryWidth) { return false; }
         if(pos.y >= 0) {
-            var idx = pos.y * this.inventoryWidth + pos.x;
+            const idx = pos.y * this.inventoryWidth + pos.x;
             if(idx < 0 || idx >= this.actualIndexes.length) { return false; }
         }
         this.cursor = { x: pos.x, y: pos.y };
@@ -87,8 +119,8 @@ worldmap.falconSelect = {
     },
     confirmSeeds: function() {
         player.nathanSeeds = [];
-        for(var i = 0; i < this.cropsToSend.length; i++) {
-            var crop = this.cropsToSend[i];
+        for(let i = 0; i < this.cropsToSend.length; i++) {
+            const crop = this.cropsToSend[i];
             player.nathanSeeds.push([crop, this.GetSeedAmountForCrop(crop)]);
             player.clearItemIfEmpty(crop);
         }
@@ -98,7 +130,7 @@ worldmap.falconSelect = {
         return true;
     },
     GetSeedAmountForCrop: function(crop) {
-        if(["coconut", "gmocorn", "notdrugs", "lotus"].indexOf(crop) >= 0) { return 2; }
+        if(["coconut", "gmocorn", "notdrugs", "lotus"].indexOf(crop) >= 0) { return 4; }
         return 10; // TODO: probably shouldn't be this for EVERY standard crop (should this be in the spreadsheet?)
     },
     click: function(pos) {
@@ -106,8 +138,8 @@ worldmap.falconSelect = {
             return this.confirmSeeds();
         } else {
             if(this.cropsToSend.length === 5) { return false; }
-            var idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
-            var item = player.inventory[this.actualIndexes[idx]];
+            const idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
+            const item = player.inventory[this.actualIndexes[idx]];
             if(item[1] === 0) { return false; }
             player.inventory[this.actualIndexes[idx]][1]--;
             this.cropsToSend.push(item[0]);
@@ -116,15 +148,15 @@ worldmap.falconSelect = {
         }
     },
     cancel: function() {
-        var last = this.cropsToSend.pop();
+        const last = this.cropsToSend.pop();
         if(last === undefined) { return false; }
         player.increaseItem(last);
         this.drawAll();
         return true;
     },
     keyPress: function(key) {
-        var pos = { x: this.cursor.x, y: this.cursor.y };
-        var isEnter = false;
+        const pos = { x: this.cursor.x, y: this.cursor.y };
+        let isEnter = false;
         switch(key) {
             case player.controls.up: pos.y--; break;
             case player.controls.left: pos.x--; break;
--- js/worldmap/inventoryClean.js
+++ js/worldmap/inventoryClean.js
@@ -16,8 +16,8 @@ worldmap.invClean = {
     DrawAll: function() {
         gfx.clearSome(this.layersToClear);
         this.actualIndexes = [];
-        var j = 0;
-        for(var i = 0; i < player.inventory.length; i++) {
+        let j = 0;
+        for(let i = 0; i < player.inventory.length; i++) {
             if(player.inventory[i][0][0] === "_" || player.inventory[i][0][0] === "!") { continue; }
             gfx.drawInventoryItem(player.inventory[i], j % this.inventoryWidth, Math.floor(j / this.inventoryWidth), "menuA");
             this.actualIndexes.push(i);
@@ -25,9 +25,9 @@ worldmap.invClean = {
             j++;
         }
         this.topPrice = 0;
-        for(var i = 0; i < this.topCrops.length; i++) {
-            gfx.drawInventoryItem(this.topCrops[i], i % this.inventoryWidth, 8 + Math.floor(i / this.inventoryWidth), "menuA");
-            if(i === (this.selectedCrop - 100)) { gfx.drawCursor(i % this.inventoryWidth, 8 + Math.floor(i / this.inventoryWidth), 0, 0, "xcursor"); }
+        for(let i = 0; i < this.topCrops.length; i++) {
+            gfx.drawInventoryItem(this.topCrops[i], i % this.inventoryWidth, 12 + Math.floor(i / this.inventoryWidth), "menuA");
+            if(i === (this.selectedCrop - 100)) { gfx.drawCursor(i % this.inventoryWidth, 12 + Math.floor(i / this.inventoryWidth), 0, 0, "xcursor"); }
             this.topPrice += GetCrop(this.topCrops[i][0]).price * this.topCrops[i][1];
         }
         if(this.cursor.x < this.inventoryWidth) { gfx.drawCursor(this.cursor.x, this.cursor.y, 0, 0); }
@@ -42,11 +42,10 @@ worldmap.invClean = {
         this.DrawAll();
     },
     mouseMove: function(pos) {
-        if(pos.x < 0 || pos.y < 0 || pos.y > 9) { return false; }
-        if(pos.y === 7) {
-            var dy = pos.y - this.cursor.y;
-            pos.y += dy;
-        }
+        if(pos.x < 0 || pos.y < 0 || pos.y > 13) { return false; }
+        if(pos.y === 9) { pos.y = 12; }
+        else if(pos.y === 11) {  pos.y = 8; }
+        if(pos.x === 4) { pos.y = this.cursor.y; }
         if(this.selectedCrop < 0) {
             if(pos.x > this.inventoryWidth) { return false; }
         } else {
@@ -59,21 +58,21 @@ worldmap.invClean = {
     HandleTrashCan: function(fromDrawAll) {
         gfx.clearLayer("tutorial");
         if(worldmap.invClean.trashInfo.length > 0) {
-            for(var i = worldmap.invClean.trashInfo.length - 1; i >= 0; i--) {
-                var ti = worldmap.invClean.trashInfo[i];
-                var trashFrame = ti.frame > 5 ? 0 : ti.frame;
+            for(let i = worldmap.invClean.trashInfo.length - 1; i >= 0; i--) {
+                const ti = worldmap.invClean.trashInfo[i];
+                const trashFrame = ti.frame > 5 ? 0 : ti.frame;
                 gfx.drawTileToGrid("animBin" + trashFrame, ti.x, ti.y, "tutorial");
                 if(!fromDrawAll) { ti.frame++; }
                 if(ti.numCoins > 0 && ti.frame % 3 === 0) {
                     ti.numCoins -= 1;
                     ti.coinStates.push({ frame: 0, x: ti.x, y: ti.y, done: false });
                 }
-                var allCoinsDone = true;
+                let allCoinsDone = true;
                 for(var j = 0; j < ti.coinStates.length; j++) {
                     var coin = ti.coinStates[j];
                     if(coin.done) { continue; }
                     allCoinsDone = false;
-                    var coinFrame = coin.frame > 3 ? 0 : coin.frame;
+                    const coinFrame = coin.frame > 3 ? 0 : coin.frame;
                     coin.done = (coin.frame > 4);
                     coin.y -= 0.25;
                     coin.frame++;
@@ -82,7 +81,7 @@ worldmap.invClean = {
                 if(allCoinsDone && ti.frame > 6) { worldmap.invClean.trashInfo.splice(i, 1); }
             }
         } else {
-            gfx.drawTileToGrid("animBin0", 4, 9, "tutorial");
+            gfx.drawTileToGrid("animBin0", 4, 13, "tutorial");
             if(worldmap.invClean.didConfirm) {
                 clearInterval(worldmap.invClean.trashIdx);
                 game.transition(worldmap.invClean, worldmap, { init: worldmap.pos, map: worldmap.mapName, noEntityUpdate: true });
@@ -90,24 +89,23 @@ worldmap.invClean = {
         }
     },
     DrawSelectInfo: function() {
-        var idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
-        var actIdx = this.actualIndexes[idx];
-        var text = "";
+        const idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
+        const actIdx = this.actualIndexes[idx];
+        let text = "";
         if(actIdx === this.selectedCrop) {
             text = GetText("inv.unselect");
         } else if(this.cursor.x === this.inventoryWidth) {
             text = GetText("inv.drop");
-            var invfo = player.inventory[this.selectedCrop];
-            var price = Math.ceil(GetCrop(invfo[0]).price * invfo[1] * 0.1);
+            const invfo = player.inventory[this.selectedCrop];
+            const price = Math.ceil(GetCrop(invfo[0]).price * invfo[1] * 0.1);
             text = text.replace(/\{0\}/g, price);
         } else {
             text = GetText("inv.swap");
         }
 
-        var y = this.cursor.y + 0.5, x = 4.5;
-        var xi = 1;
-        var width = gfx.getTextWidth(text) + 20;
-        var xiimax = x + Math.ceil(width / 64);
+        const y = this.cursor.y + 0.5, x = 4.5;
+        let xi = 1, width = gfx.getTextWidth(text) + 20;
+        let xiimax = x + Math.ceil(width / 64);
         while(xiimax > 14) { x -= 1; xiimax = x + Math.ceil(width / 64); }
         gfx.drawSprite("sheet", 41, 15, x * 16, 2 + y * 16, "menuOverBlack");
         while(width > 128) {
@@ -118,19 +116,19 @@ worldmap.invClean = {
         gfx.drawText(text, 7 + x * 16, 10.5 + y * 16, undefined, undefined, "menutextOverBlack");
     },
     ConfirmSelection: function() {
-        this.trashInfo.push({ frame: 0, coinStates: [], x: 4, y: 9, numCoins: Math.min(10, Math.ceil(this.topPrice / 30)) });
+        this.trashInfo.push({ frame: 0, coinStates: [], x: 4, y: 13, numCoins: Math.min(10, Math.ceil(this.topPrice / 30)) });
         player.monies += this.topPrice;
         this.didConfirm = true;
     },
     click: function(pos) {
         if(this.didConfirm) { return false; }
         if(this.cursor.x === this.inventoryWidth) { return this.ConfirmSelection(); }
-        var newidx = -1;
-        if(this.cursor.y < 7) {
-            var idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
+        let newidx = -1;
+        if(this.cursor.y < 10) {
+            const idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
             if(this.actualIndexes[idx] !== undefined) { newidx = this.actualIndexes[idx]; }
-        } else if(this.cursor.y > 7) {
-            var idx = (this.cursor.y - 8) * this.inventoryWidth + this.cursor.x;
+        } else if(this.cursor.y > 10) {
+            const idx = (this.cursor.y - 12) * this.inventoryWidth + this.cursor.x;
             if(idx < this.topCrops.length) { newidx = idx; }
             newidx += 100;
         } else { return false; }
@@ -144,27 +142,27 @@ worldmap.invClean = {
             this.selectedCrop = -1;
         } else {
             if(this.selectedCrop >= 100 && newidx === -1) { // putting tossable into empty inventory space
-                var takenCrop = this.topCrops.splice(this.selectedCrop - 100, 1)[0];
+                const takenCrop = this.topCrops.splice(this.selectedCrop - 100, 1)[0];
                 if(!player.increaseItem(takenCrop[0], takenCrop[1])) { this.topCrops.push(takenCrop); }
             } else if(this.selectedCrop < 100 && newidx === 99) { // putting inventory item into empty tossable space
-                var takenCrop = player.inventory.splice(this.selectedCrop, 1)[0];
+                const takenCrop = player.inventory.splice(this.selectedCrop, 1)[0];
                 this.topCrops.push(takenCrop);
             } else if(this.selectedCrop >= 100 && newidx >= 100) { // sorting the tossables for some reason
                 newidx -= 100;
                 this.selectedCrop -= 100;
-                var temp = this.topCrops[newidx];
+                const temp = this.topCrops[newidx];
                 this.topCrops[newidx] = this.topCrops[this.selectedCrop];
                 this.topCrops[this.selectedCrop] = temp;
             } else if(this.selectedCrop < 100 && newidx < 100) { // sorting inventory for some reason
-                var temp = player.inventory[newidx];
+                const temp = player.inventory[newidx];
                 player.inventory[newidx] = player.inventory[this.selectedCrop];
                 player.inventory[this.selectedCrop] = temp;
             } else if(this.selectedCrop < 100 && newidx >= 100) { // swapping tossable with inventory
-                var temp = this.topCrops[newidx - 100];
+                const temp = this.topCrops[newidx - 100];
                 this.topCrops[newidx - 100] = player.inventory[this.selectedCrop];
                 player.inventory[this.selectedCrop] = temp;
             } else if(this.selectedCrop >= 100 && newidx < 100) { // swapping inventory with tossable
-                var temp = player.inventory[newidx];
+                const temp = player.inventory[newidx];
                 player.inventory[newidx] = this.topCrops[this.selectedCrop - 100];
                 this.topCrops[this.selectedCrop - 100] = temp;
             }
@@ -174,8 +172,8 @@ worldmap.invClean = {
         return true;
     },
     keyPress: function(key) {
-        var pos = { x: this.cursor.x, y: this.cursor.y };
-        var isEnter = false;
+        const pos = { x: this.cursor.x, y: this.cursor.y };
+        let isEnter = false;
         switch(key) {
             case player.controls.up: pos.y--; break;
             case player.controls.left: pos.x--; break;
@@ -193,11 +191,11 @@ worldmap.invClean = {
         }
     },
     DrawConfirmButton: function() {
-        var text = GetText("falconConfirm");
-        var y = 9, x = 7, xi = 1;
-        var width = gfx.getTextWidth(text) + 20;
-        var xiimax = x + Math.ceil(width / 64);
-        var isSelected = this.cursor.x === this.inventoryWidth;
+        const text = GetText("falconConfirm");
+        let y = 13, x = 7, xi = 1;
+        let width = gfx.getTextWidth(text) + 20;
+        let xiimax = x + Math.ceil(width / 64);
+        const isSelected = this.cursor.x === this.inventoryWidth;
         while(xiimax > 14) { x -= 1; xiimax = x + Math.ceil(width / 64); }
         gfx.drawSprite("sheet", isSelected ? 41 : 39, isSelected ? 15 : 16, x * 16, 2 + y * 16, "menuOverBlack");
         while(width > 128) {
@@ -209,42 +207,43 @@ worldmap.invClean = {
         gfx.drawText(text, 7 + x * 16, 10.5 + y * 16, undefined, undefined, "menutextOverBlack");
     },
     SetCrops: function() {
-        var rowYs = [0.25, 1.5, 2.75, 6.25, 7.5, 8.75];
-        var rowTextYs = [16, 0, 0, 113, 0, 0];
-        gfx.drawInfobox(11, 4, 0);
-        gfx.drawInfobox(11, 4, 4);
-        gfx.drawInfobox(10, 2, 8);
-        gfx.drawTileToGrid("recycleArrow", 4, 8, "menuA");
+        const rowYs = [0.25, 1.5, 2.75, 6.25, 7.5, 8.75];
+        const rowTextYs = [16, 0, 0, 113, 0, 0];
+        gfx.drawInfobox(12, 4, 0);
+        gfx.drawInfobox(12, 4, 6);
+        gfx.drawInfobox(11, 2, 12);
+        gfx.drawTileToGrid("recycleArrow", 4, 12, "menuA");
 
-        gfx.drawWrappedText(GetText("fullSelect"), 70, 75, 160);
-        gfx.drawWrappedText(GetText("fullToss").replace(/\{0\}/g, this.topPrice), 90, 140, 130);
+        gfx.drawWrappedText(GetText("fullSelect"), 70, 105, 170);
+        gfx.drawWrappedText(GetText("fullToss").replace(/\{0\}/g, this.topPrice), 90, 204, 140);
         this.DrawConfirmButton();
  
-        var crop = null;
-        if(this.cursor.y < 7) {
-            var idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
-            var actIdx = this.actualIndexes[idx];
-            var item = player.inventory[actIdx];
+        let crop = null;
+        if(this.cursor.x === 4) { return; }
+        if(this.cursor.y < 10) {
+            const idx = this.cursor.y * this.inventoryWidth + this.cursor.x;
+            const actIdx = this.actualIndexes[idx];
+            const item = player.inventory[actIdx];
             if(item !== undefined) { crop = GetCrop(item[0]); }
-        } else if(this.cursor.y > 7) {
-            var idx = (this.cursor.y - 8) * this.inventoryWidth + this.cursor.x;
-            var item = this.topCrops[idx];
+        } else if(this.cursor.y > 10) {
+            const idx = (this.cursor.y - 12) * this.inventoryWidth + this.cursor.x;
+            const item = this.topCrops[idx];
             if(item !== undefined) { crop = GetCrop(item[0]); }
         }
 
         if(crop !== null) { this.DrawCropText(crop, rowYs, rowTextYs, 0); }
     },
     DrawCropText: function(crop, rowYs, rowTextYs, yOffset) {
-        var leftMostX = 4.25;
-        var rightMostX = 14;
-        var leftMostTextX = 88;
-        var starStartX = leftMostX + 1, starDx = 1;
+        const leftMostX = 4.25;
+        const rightMostX = 14;
+        const leftMostTextX = 88;
+        const starStartX = leftMostX + 1, starDx = 1;
 
         // Row 0
         gfx.drawText(crop.displayname, leftMostTextX, rowTextYs[yOffset], undefined, 32);
         gfx.drawTileToGrid(crop.name, leftMostX, rowYs[yOffset], "menutext");
 
-        var cropSprite = "dirt";
+        let cropSprite = "dirt";
         switch(crop.type) {
             case "bee": cropSprite = "_beehive"; break;
             case "spear":
@@ -261,37 +260,35 @@ worldmap.invClean = {
         gfx.drawItemNumber(crop.size, leftMostX + 9.5, rowYs[yOffset], "menutext", true);
 
         // Row 1
-        var seasons = ["spring", "summer", "autumn", "winter"];
-        for(var i = 0; i < 4; i++) {
+        const seasons = ["spring", "summer", "autumn", "winter"];
+        for(let i = 0; i < 4; i++) {
             gfx.drawTileToGrid(seasons[i] + crop.seasons[i], leftMostX + 6.75 + i, rowYs[1 + yOffset], "menutext");
         }
 
         gfx.drawTileToGrid("inv_power", leftMostX, rowYs[1 + yOffset], "menutext");
-        var numStars = crop.power / 2;
+        const numStars = crop.power / 2;
         if(numStars > 5) {
-            for(var i = 0; i < 5; i++) {
+            for(let i = 0; i < 5; i++) {
                 gfx.drawTileToGrid("starMax", starStartX + i * starDx, rowYs[1 + yOffset], "menutext");
             }
         } else {
-            for(var i = 0; i < numStars; i++) {
+            for(let i = 0; i < numStars; i++) {
                 gfx.drawTileToGrid("starFull", starStartX + i * starDx, rowYs[1 + yOffset], "menutext");
             }
             if(numStars % 1 !== 0) { gfx.drawTileToGrid("starHalf", starStartX + (numStars - 0.5) * starDx, rowYs[1 + yOffset], "menutext"); }
-            for(var i = Math.ceil(numStars); i < 5; i++) {
+            for(let i = Math.ceil(numStars); i < 5; i++) {
                 gfx.drawTileToGrid("starNone", starStartX + i * starDx, rowYs[1 + yOffset], "menutext");
             }
         }
 
         // Row 2
         gfx.drawTileToGrid("inv_time", leftMostX, rowYs[2 + yOffset], "menutext");
-        var timeNum = crop.time;
         if(crop.time === 999 || crop.time === -1) { // TODO: -1 vs 999 what is the diff?
             gfx.drawTileToGrid("bigNum?", leftMostX + 1, rowYs[2 + yOffset], "menutext");
         }  else {
             gfx.drawBigNumber(crop.time, leftMostX + 1, rowYs[2 + yOffset], "menutext");
         }
         if(crop.respawn > 0) {
-            //timeNum = crop.respawn;
             gfx.drawTileToGrid("inv_regrow", leftMostX + 2, rowYs[2 + yOffset], "menutext");
             if(crop.respawn === 999 || crop.respawn === -1) {
                 gfx.drawTileToGrid("bigNum?", leftMostX + 3, rowYs[2 + yOffset], "menutext");
@@ -300,14 +297,14 @@ worldmap.invClean = {
             }
         }
 
-        var bonusesToPush = [];
+        const bonusesToPush = [];
         if(crop.waterResist) { bonusesToPush.push("waterIco" + crop.waterResist); }
         if(crop.fireResist) { bonusesToPush.push("fireIco" + crop.fireResist); }
         if(crop.stickChance) { bonusesToPush.push("stunIco" + crop.stickChance); }
         if(crop.saltResist) { bonusesToPush.push("saltIco" + crop.saltResist); }
         if(crop.saltClean) { bonusesToPush.push("saltIcoX"); }
         if(crop.animal) { bonusesToPush.push("animal" + crop.animal); }
-        for(var i = 0; i < bonusesToPush.length; i++) {
+        for(let i = 0; i < bonusesToPush.length; i++) {
             gfx.drawTileToGrid(bonusesToPush[i], rightMostX - 0.25 - i, rowYs[2 + yOffset], "menutext");
         }
     }
