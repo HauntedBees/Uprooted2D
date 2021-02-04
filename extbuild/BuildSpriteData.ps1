5542352453ba5a8a6c94f8849891da3a6550c678
Haunted Bees <fench@hauntedbees.com>
2021-02-03 23:27:44 -0800

    rugrats BACK IN ACTION bay-BEEEE

--- extbuild/BuildSpriteData.ps1
+++ extbuild/BuildSpriteData.ps1
@@ -4,7 +4,7 @@ $rootpath = Resolve-Path ..;
 if($which.Contains("S")) {
     Write-Host "Converting Details_BigSprites.ods and Details_SmallSprites to spritedata.js";
     Write-Host "Starting Details_BigSprites.ods Conversion";
-    & "C:\Program Files (x86)\LibreOffice 5\program\scalc.exe" --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,0,,,,true" --outdir ".\temp" "Details_BigSprites.ods" | Out-Null;
+    & "C:\Program Files\LibreOffice\program\scalc.exe" --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,0,,,,true" --outdir ".\temp" "Details_BigSprites.ods" | Out-Null;
 	$csv = Import-CSV ".\temp\Details_BigSprites.csv" -Header 0, 1, 2, 3, 4, 5, 6, 7, 8;
     $out = [System.IO.StreamWriter] "$rootpath\js\gamedata\spritedata.js";
     $aliases = @{};
@@ -35,10 +35,10 @@ if($which.Contains("S")) {
     }
     
     Write-Host "Starting Details_SmallSprites.ods Conversion";
-    & "C:\Program Files (x86)\LibreOffice 5\program\scalc.exe" --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,0,,,,true" --outdir ".\temp" "Details_SmallSprites.ods" | Out-Null;
+    & "C:\Program Files\LibreOffice\program\scalc.exe" --headless --convert-to csv:"Text - txt - csv (StarCalc)":"44,34,0,,,,true" --outdir ".\temp" "Details_SmallSprites.ods" | Out-Null;
 	$csv = Import-CSV ".\temp\Details_SmallSprites.csv" -Header 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30;
     $aliases = @{}; $addFives = @{};
-    $row = 33;
+    $row = 36;
     while($true) {
         if($csv[$row].0 -eq $null) { break; }
         $key = $csv[$row].1;
@@ -52,7 +52,7 @@ if($which.Contains("S")) {
         }
         $row += 1;
     }
-    for($y = 0; $y -lt 31; $y++) {
+    for($y = 0; $y -lt 34; $y++) {
         for($x = 0; $x -lt 31; $x++) {
             $val = $csv[$y].$x;
             if($val -eq "") { continue; }
