bffcb80c0b5dcee14f150bd38b4ba23359c3feef
Haunted Bees <fench@hauntedbees.com>
2021-03-21 13:56:45 -0700

    how was that not already in there

--- tools/Cordova.js
+++ tools/Cordova.js
@@ -17,7 +17,7 @@ const action = args[0].toLowerCase(), all = action === "all";
 if(all || action === "copy") {
     const root = path.join(__dirname, "../");
     const croot = path.join(root, "cordova/www/");
-    const copies = ["img", "imggb", "imghq4x", "imgs4x", "styles"];
+    const copies = ["img", "imggb", "imghq4x", "imgs4x", "styles", "sound"];
     copies.forEach(f => fs.copySync(path.join(root, f), path.join(croot, f)));
     fs.copySync(path.join(root, "js/lib"), path.join(croot, "js"));
     fs.copySync(path.join(root, "out.js"), path.join(croot, "js/out.js"));
