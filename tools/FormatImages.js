27b2d5e03bd05ffe6d124f8e1c7cb5e2b40d7621
Haunted Bees <fench@hauntedbees.com>
2021-03-21 13:56:01 -0700

    s4x is back bayBEE

--- tools/FormatImages.js
+++ tools/FormatImages.js
@@ -1,10 +1,31 @@
 const ffmpeg = require("fluent-ffmpeg");
 const path = require("path");
 const fs = require("fs");
+const sharp = require("sharp");
+const imagemin = require("imagemin");
+const imageminPngquant = require("imagemin-pngquant");
+const { spawn } = require("child_process");
+const s2xpath = "C:/scale2x/scalex.exe";
+
+const Resize = async function(inPath, tempPath, outPath, callback) {
+    const img = sharp(inPath);
+    return img.metadata().then(metadata => {
+        img.extend({ top: 0, left: 0, right: 0, bottom: 0, background:"#00000000" })
+            .resize({ width: metadata.width * 0.25, kernel: sharp.kernel.nearest }).toFile(tempPath, () => {
+            imagemin([tempPath], {
+                destination: tempPath,
+                plugins: [imageminPngquant()]
+            }).then(() => callback(tempPath, outPath));
+        });
+    });
+}
+
 const Filter = async function(type, subpath, specifics) {
 	const inpath = path.join(__dirname, "../img", subpath);
+	const temppath = path.join(__dirname, `temp/img${type}`, subpath);
 	const outpath = path.join(__dirname, `../img${type}`, subpath);
 	const files = fs.readdirSync(inpath);
+	if(!fs.existsSync(temppath)) { fs.mkdirSync(temppath); }
 	if(!fs.existsSync(outpath)) { fs.mkdirSync(outpath); }
 
 	const Processes = {
@@ -15,8 +36,14 @@ const Filter = async function(type, subpath, specifics) {
 			cmd.on("end", () => { console.log(`Processed ${subpath}/${f}`); resolve(true); });
 			cmd.on("error", () => reject());
 			cmd.run();
+		}),
+		"s4xe": f => new Promise((resolve, reject) => {
+			Resize(path.join(inpath, f), path.join(temppath, f), path.join(outpath, f), (infile, outfile) => {
+				const s4x = spawn(s2xpath, ["-k", "4", infile, outfile]);
+				s4x.on("close", () => { console.log(`Processed ${subpath}/${f}`); resolve(true) });
+				s4x.stderr.on("data", () => reject());
+			});
 		})
-		// TODO: scale4x?
 	};
 
 	for(let i = 0; i < files.length; i++) {
